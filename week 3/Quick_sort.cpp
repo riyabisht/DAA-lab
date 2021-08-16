41b5ce86332acf11a6f569deda0147521265228a
Riya Bisht <riyabisht152@gmail.com>
2021-08-16 11:10:36 +0530

    changes to weak 3 quick sort

--- week 3/Quick_sort.cpp
+++ week 3/Quick_sort.cpp
@@ -57,15 +57,15 @@ int main()
         cin >> n;
         n=n+1;
         int *a = new int[n];
-        for (int i = 0; i < n; i++)
+        for (int i = 0; i < n-1; i++)
             cin >> a[i];
 
-        a[n] = INT32_MAX;
-        Quick_sort(a, 0, n);
+        a[n-1] = INT32_MAX;
+        Quick_sort(a, 0, n-1);
 
 
         int flag = 0;
-        for (int i = 0; i < n - 1; i++)
+        for (int i = 0; i < n - 2; i++)
         {
             if (a[i] == a[i + 1])
             {
